0d37610ac255517b6e352f30a5ba4923def54e2b
Jack <jckwind11@gmail.com>
2025-04-29 19:20:29 -0400

    successful split between c++ and python

--- src/proxy_inference_engine/__init__.py
+++ src/proxy_inference_engine/__init__.py
@@ -5,6 +5,6 @@ os.environ.setdefault("TRANSFORMERS_NO_ADVISORY_WARNINGS", "1")
 
 from proxy_inference_engine.engine import InferenceEngine
 
-pie_core = import_module("pie_core")
+pie_core = import_module("proxy_inference_engine.pie_core")
 
 __all__ = ["InferenceEngine", "pie_core"]
